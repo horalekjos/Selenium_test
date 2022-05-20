93a5b11ef954009dce0fc4c18a6e63f28003e5dd
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-05-20 11:56:40 +0200

    Add files via upload

---
+++ SeleniumConfig.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+
+
+namespace UHK_Selenium_Adam_Kucera
+{
+    public class SeleniumConfig
+    {
+
+
+        String hubAdress = "http://localhost:4444";
+        ChromeOptions options = new ChromeOptions();
+
+        public SeleniumConfig()
+        {
+
+        }
+
+        public void PrepareDriver(string name = "Selenium Test")
+        {
+            options.AddArguments("--headless");
+            options.AddArguments("--start-maximized");
+            remoteDriver = new RemoteWebDriver(new Uri(hubAdress), options);
+        }
+
+
+        public RemoteWebDriver remoteDriver { get; set; } = default!;
+
+    }
+}
